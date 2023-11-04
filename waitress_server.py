e23844a904a465e4876943aa944dbd329202dcd5
osama558 <149424297+osama558@users.noreply.github.com>
2023-11-04 04:21:41 +0000

    Update waitress_server.py

--- waitress_server.py
+++ waitress_server.py
@@ -1,5 +1,5 @@
 from waitress import serve
 from app import app
 
-if __name__ == "__main__":
-    serve(app, host="https://osama558.github.io/flask/", port=443)
+#if __name__ == "__main__":
+serve(app, host="https://osama558.github.io/flask/", port=443)
